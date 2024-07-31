e7995aaa89c3f17e98b8b641f4551f6201643976
We1337-alpha <weuser149@gmail.com>
2024-07-31 18:05:49 +0200

    Init

---
+++ core/home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from datetime import datetime, timedelta
+
+def countdown(request):
+    target_date = datetime(2024, 7, 30, 0, 0, 0)
+    now = datetime.now()
+    time_remaining = target_date - now
+
+    context = {
+        'days': f"{time_remaining.days:02}",
+        'hours': f"{time_remaining.seconds // 3600:02}",
+        'minutes': f"{(time_remaining.seconds % 3600)} // 60:02",
+        'seconds': f"{time_remaining.seconds % 60:02}",
+    }
+
+    return render(request, 'countdown.html', context)
